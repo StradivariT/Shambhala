a1f23eb2dea6cc8589649629af35e7306f99e1b2
FerVT <fer.vargas.torres@gmail.com>
2018-02-05 16:10:39 -0600

    Change some middleware and jwt configs

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use JWTAuth;
-use App\User;
 
 class UsersController extends Controller
 {
@@ -14,9 +13,9 @@ class UsersController extends Controller
 
         try {
             if(!$token = JWTAuth::attempt($credentials))
-                return response()->json(['error' => 'La información ingresada es incorrecta'], 401);
+                return response()->json(['error' => 'Invalid credentials'], 401);
         } catch(JWTException $error) {
-            return response()->json(['error' => 'Un error inesperado ocurrió al tratar de iniciar sesión, intenta de nuevo'], 500);
+            return response()->json(['error' => $error], 500);
         }
 
         return response()->json(['token' => $token], 200);

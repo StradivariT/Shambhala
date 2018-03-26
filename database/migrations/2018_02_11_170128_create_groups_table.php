e7aa9e12739442c5f735cbcdb0d6ee79a71437e7
FerVT <fer.vargas.torres@gmail.com>
2018-03-25 18:58:52 -0600

    Update groups migration

--- database/migrations/2018_02_11_170128_create_groups_table.php
+++ database/migrations/2018_02_11_170128_create_groups_table.php
@@ -19,6 +19,12 @@ class CreateGroupsTable extends Migration
             $table->integer('course_id')->unsigned();
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->string('name')->unique();
+            $table->string('participants_file_name');
+            $table->string('participants_file_storage');
+            $table->string('incidents_file_name');
+            $table->string('incidents_file_storage');
+            $table->string('evaluations_file_name');
+            $table->string('evaluations_file_storage');
         });
     }
 

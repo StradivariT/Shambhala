e571722333fdf60c7dc53522d704d103402beca7
FerVT <fer.vargas.torres@gmail.com>
2018-03-30 16:39:12 -0600

    Students Controller: General improvements

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,45 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Student;
 use App\Group;
 
 class StudentsController extends Controller {
-    public function index($id) {
-        $group = Group::find($id);
+    public function index($groupId) {
+        $group = Group::find($groupId);
 
         if(empty($group))
-            return response()->json(['message' => 'Group not found, invalid ID'], 400);
+            return response()->json('Group not found, invalid ID', 400);
 
-        $students = Student::where('group_id', '=', $group->id)->orderBy('number', 'ASC')->get();
+        $students = Student::select('id', 'name', 'number')->where('group_id', '=', $group->id)->orderBy('number', 'ASC')->get();
 
         if($students->isEmpty())
-            return response()->json(['message' => 'Students not found for group: ' . $id . '.'], 404);
+            return response()->json('Students not found for group: ' . $groupId, 404);
 
-        return response()->json(['students' => $students], 200);
+        return response()->json($students, 200);
     }
 
-    public function store($id, Request $request) {
+    public function store(Request $request, $groupId) {
+        $newStudentInfo = $request->except(['token']);
+
         try {
             $newStudent = new Student;
-            $newStudent->name = $request->input('newStudentName');
-            $newStudent->number = $request->input('newStudentNumber');
-            $newStudent->group_id = $id;
+
+            $newStudent->group_id = $groupId;
+            $newStudent->name = $newStudentInfo['name'];
+            $newStudent->number = $newStudentInfo['number'];
+
             $newStudent->save();
-        } catch(Exception $e) {
-            return response()->json(['message' => 'Unexpected student error', 'error' => $e], 500);
+
+            $newStudentInfo['id'] = $newStudent->id;
+        } catch(Exception $error) {
+            //TODO: Log $error.
+            return response()->json('Unexpected student error', 500);
         }
 
-        return response()->json(['newStudent' => $newStudent], 200);
+        return response()->json($newStudentInfo, 200);
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $studentId) {
         $data = $request->except(['token']);
 
         try {
-            Student::where('id', $id)->update($data);
-        } catch(Exception $e) {
-            return response()->json(['message' => 'Unexpected student error', 'error' => $e], 500);
+            Student::where('id', $studentId)->update($data);
+        } catch(Exception $error) {
+            //TODO: Log $error
+            return response()->json('Unexpected student error', 500);
         }
 
         return response()->json(['name' => $data['name'], 'number' => $data['number']], 200);

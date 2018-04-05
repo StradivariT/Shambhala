14f9f556bcd8b6970c0ed6586550df45b235a336
FerVT <fer.vargas.torres@gmail.com>
2018-04-05 00:02:51 -0500

    Fix seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\DB;
 class UsersTableSeeder extends Seeder {
     public function run() {
         DB::table('users')->insert([
-            ['email' => 'fer.vargas.torres@gmail.com', 'password' => bcrypt('8reakthe5tr34k')],
-            ['email' => 'gil_5140@hotmail.com',        'password' => bcrypt('santodio5')]
+            ['email' => 'fer.vargas.torres@gmail.com', 'password' => '$2y$10$1k/B5Sb4i/s7Wwa3.kzrze7MbZ7R7XiSBRU.73kRyl1SEEbOFZx/K'],
+            ['email' => 'gildatn@gmail.com',           'password' => '$2y$10$iEI/KfHx4pntqA5bgHFjK.7onrHqgUEvTm/t4z5Awu7Sz1VJLIMZS']
         ]);
     }
 }
